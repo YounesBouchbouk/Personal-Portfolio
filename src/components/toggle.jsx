d8a68979d032f8f4d215357a53e1d5ab038d8aaa
Younes Bouchbouk <younesbouchbouk.py@gmail.com>
2022-08-07 17:55:22 +0200

    deploy fix toggle

--- src/components/toggle.jsx
+++ src/components/toggle.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react"
 import { ThemeContext } from "../mode/modeContext"
 
 const Toggle = () => {
-  const { theme, setTheme } = useContext(ThemeContext || "dark")
+  const { theme, setTheme } = useContext(ThemeContext || {})
   return (
     <div>
       {theme === "dark" ? (
