48505b63439656769fd0fa520293352d99445b22
Younes Bouchbouk <younesbouchbouk.py@gmail.com>
2023-09-21 18:19:59 +0100

    add google an

---
+++ src/components/Annonce.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Annonce = () => {
+  return (
+    <div className="w-full bg-white-c border-b-[1px] dark:bg-black items-center justify-center flex fixed shadow-black dark:shadow-yellow-600 shadow-lg">
+      <p className="dark:text-white font-bold ">
+        {" "}
+        Currently i'm looking for an end-of-studies internship opportunity in
+        software development.
+      </p>
+    </div>
+  )
+}
+
+export default Annonce
